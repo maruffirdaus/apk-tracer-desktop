5358fcdbacd6a6b2380604c29912ec5ee246a77b
wildanniam <wildanniam4@gmail.com>
2025-10-19 15:25:23 +0700

    build for macos

--- composeApp/src/jvmMain/kotlin/app/apktracer/common/utils/AvdUtil.kt
+++ composeApp/src/jvmMain/kotlin/app/apktracer/common/utils/AvdUtil.kt
@@ -12,62 +12,58 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 
 class AvdUtil() {
-    suspend fun duplicate(avdIni: String): String = withContext(Dispatchers.IO) {
-        val file = PlatformFile(avdIni)
-        val lines = file.readString().lines().toMutableList()
-        for (i in lines.indices) {
-            if (lines[i].contains("path=")) {
-                val sourceDir = PlatformFile(lines[i].substringAfter("path="))
-                val destinationDir = PlatformFile(
-                    sourceDir.parent() ?: PlatformFile("/"),
-                    "${sourceDir.name.substringBefore(".")}_Copy.avd"
-                )
-                sourceDir.file.copyRecursively(destinationDir.file)
-                lines[i] = lines[i].replace(".avd", "_Copy.avd")
+    suspend fun duplicate(avdIni: String): String =
+            withContext(Dispatchers.IO) {
+                val file = PlatformFile(avdIni)
+                val lines = file.readString().lines().toMutableList()
+                for (i in lines.indices) {
+                    if (lines[i].contains("path=")) {
+                        val sourceDir = PlatformFile(lines[i].substringAfter("path="))
+                        val destinationDir =
+                                PlatformFile(
+                                        sourceDir.parent() ?: PlatformFile("/"),
+                                        "${sourceDir.name.substringBefore(".")}_Copy.avd"
+                                )
+                        sourceDir.file.copyRecursively(destinationDir.file)
+                        lines[i] = lines[i].replace(".avd", "_Copy.avd")
+                    }
+                    if (lines[i].contains("path.rel")) {
+                        lines[i] = lines[i].replace(".avd", "_Copy.avd")
+                    }
+                }
+                file.parent()?.let {
+                    val duplicatedFile = PlatformFile(it, "${file.nameWithoutExtension}_Copy.ini")
+                    duplicatedFile.writeString(lines.joinToString("\n"))
+                    return@withContext duplicatedFile.absolutePath()
+                }
+                return@withContext "${file.nameWithoutExtension}_Copy.ini"
             }
-            if (lines[i].contains("path.rel")) {
-                lines[i] = lines[i].replace(".avd", "_Copy.avd")
+
+    suspend fun start(avdIni: String) =
+            withContext(Dispatchers.IO) {
+                ProcessBuilder(
+                                "/Users/wildanniam/Library/Android/sdk/emulator/emulator",
+                                "-avd",
+                                PlatformFile(avdIni).nameWithoutExtension
+                        )
+                        .start()
             }
-        }
-        file.parent()?.let {
-            val duplicatedFile = PlatformFile(
-                it,
-                "${file.nameWithoutExtension}_Copy.ini"
-            )
-            duplicatedFile.writeString(lines.joinToString("\n"))
-            return@withContext duplicatedFile.absolutePath()
-        }
-        return@withContext "${file.nameWithoutExtension}_Copy.ini"
-    }
 
-    suspend fun start(avdIni: String) = withContext(Dispatchers.IO) {
-        ProcessBuilder(
-            "cmd",
-            "/c",
-            "start",
-            "emulator",
-            "-avd",
-            PlatformFile(avdIni).nameWithoutExtension
-        ).start()
-    }
-
-    suspend fun kill() = withContext(Dispatchers.IO) {
-        val process = ProcessBuilder(
-            "adb",
-            "emu",
-            "kill"
-        ).start()
-        process.waitFor()
-    }
-
-    suspend fun delete(avdIni: String) = withContext(Dispatchers.IO) {
-        val file = PlatformFile(avdIni)
-        val lines = file.readString().lines().toMutableList()
-        for (line in lines) {
-            if (line.contains("path=")) {
-                PlatformFile(line.substringAfter("path=")).file.deleteRecursively()
+    suspend fun kill() =
+            withContext(Dispatchers.IO) {
+                val process = ProcessBuilder("adb", "emu", "kill").start()
+                process.waitFor()
             }
-        }
-        file.delete()
-    }
+
+    suspend fun delete(avdIni: String) =
+            withContext(Dispatchers.IO) {
+                val file = PlatformFile(avdIni)
+                val lines = file.readString().lines().toMutableList()
+                for (line in lines) {
+                    if (line.contains("path=")) {
+                        PlatformFile(line.substringAfter("path=")).file.deleteRecursively()
+                    }
+                }
+                file.delete()
+            }
 }
